2bba7c2e0a5904f44ac668aec068386024871e21
Michael Dijkstra <micdijkstra@gmail.com>
2014-02-15 12:08:07 +1100

    Code up initial template
    + Display nav bar
    + Display weather information
    + Display products
    + Display Download button
    + Fade out weather information on scroll like iPhone app
    + Stack products with JS
    + Hover to show product information
    + Optimise for palm (320px), lap (768px), desk (1500px)

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var colCount = 0;
+var colWidth = 0;
+var margin = 0;
+var windowWidth = 0;
+var blocks = [];
+
+$(function(){
+  $(window).resize(setupBlocks);
+});
+
+function setupBlocks() {
+  windowWidth = $('.productPage').width();
+  colWidth = $('.block').outerWidth();
+  blocks = [];
+  console.log(blocks);
+  colCount = Math.floor(windowWidth/(colWidth+margin*2));
+  for(var i=0;i<colCount;i++){
+    blocks.push(margin);
+  }
+  positionBlocks();
+}
+
+function positionBlocks() {
+  $('.block').each(function(){
+    var min = Array.min(blocks);
+    var index = $.inArray(min, blocks);
+    var leftPos = margin+(index*(colWidth+margin));
+    $(this).css({
+      'left':leftPos+'px',
+      'top':min+'px',
+      'z-index':'-1'
+    });
+    blocks[index] = min+$(this).outerHeight()+margin;
+  });
+}
+
+function fadeWeather() {
+  var scrollTop = $(this).scrollTop();
+  var fadeHeight = 245;
+  if ($(window).width() >= 1500) {
+    var fadeHeight = 527;
+  } else if ($(window).width() >= 768) {
+    var fadeHeight = 408;
+  }
+
+  var fadeOpacity;
+  if (scrollTop < 0) {
+    fadeOpacity = 1;
+  } else if (scrollTop/fadeHeight >= 0) {
+    fadeOpacity = 1-scrollTop/fadeHeight;
+  } else {
+    fadeOpacity = 0;
+  }
+
+  if (fadeOpacity >= 0) {
+    $('.weatherBody .fade, .weatherFoot .fade').css('opacity', fadeOpacity);
+    $('.weatherBody, .weatherFoot').css('display', 'block');
+  } else {
+    $('.weatherBody, .weatherFoot').css('display', 'none');
+  }
+}
+
+// Function to get the Min value in Array
+Array.min = function(array) {
+    return Math.min.apply(Math, array);
+};
+
+$( document ).ready(function() {
+  setupBlocks();
+  $(window).scroll(function(event) {
+    fadeWeather();
+  });
+});
